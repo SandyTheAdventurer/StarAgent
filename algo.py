45b2ae495dfdced1625f462409c48af99faae34f
sandy <sandytheadventurer@gmail.com>
2025-03-05 00:02:15 +0530

    Added Available actions

--- algo.py
+++ algo.py
@@ -1,37 +1,27 @@
 from pysc2.agents import base_agent
-from pysc2.lib import actions, features
-from pysc2.env import sc2_env
-from absl import app
+from pysc2.lib import actions
+
+import torch
+import torch.nn as nn
+from torch import tensor
+
+SELECT_POINT=actions.FUNCTIONS.select_point.id
+SELECT_RECT=actions.FUNCTIONS.select_rect.id
+SELECT_ARMY=actions.FUNCTIONS.select_army.id
+MOVE_CAMERA=actions.FUNCTIONS.move_camera.id
+ATTACK_MINIMAP=actions.FUNCTIONS.Attack_minimap.id
+TRAIN_DRONE_QUICK=actions.FUNCTIONS.Train_Drone_quick.id
+BUILD_SPAWNPOOL=actions.FUNCTIONS.Build_SpawningPool_screen.id
+TRAIN_ZERGLING=actions.FUNCTIONS.Train_Zergling_quick.id
+TRAIN_OVERLORD=actions.FUNCTIONS.Train_Overlord_quick.id
+NO_OP=actions.FUNCTIONS.no_op.id
 
 class AgentZero(base_agent.BaseAgent):
+    def __init__(self):
+        super().__init__()
+        self.stack=torch.nn.Sequential(
+            nn.Conv2d()
+        )
     def step(self, obs):
         super(AgentZero, self).step(obs)
         return actions.FUNCTIONS.no_op()
-
-def main(args):
-    agent = AgentZero()
-    try:
-        env= sc2_env.SC2Env(
-            map_name="Simple128",
-            players=[sc2_env.Agent(sc2_env.Race.zerg),
-                    sc2_env.Bot(sc2_env.Race.random,
-                    sc2_env.Difficulty.very_easy)],
-            agent_interface_format=features.AgentInterfaceFormat(feature_dimensions=features.Dimensions(screen=84, minimap=64)),
-            step_mul=16, game_steps_per_episode=0, visualize=True)
-        
-        agent.setup(env.observation_spec(), env.action_spec())
-        
-        timesteps = env.reset()
-        agent.reset()
-        
-        while True:
-          step_actions = [agent.step(timesteps[0])]
-          if timesteps[0].last():
-            break
-          timesteps = env.step(step_actions)
-      
-    except KeyboardInterrupt:
-        pass
-  
-if __name__ == "__main__":
-  app.run(main)
